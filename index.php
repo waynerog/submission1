0e7bb1cb358428ae6ef1cefa7c57499ae00ca4fb
Rifqi Afif <rifqiafif007@gmail.com>
2019-06-17 13:46:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,85 +1,87 @@
 <html>
- <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
- </head>
- <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
- <?php
-    $host = "rifazures.database.windows.net";
-    $user = "waynerog";
-    $pass = "Alkmenes1197";
-    $db = "Pendaftaran";
-
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-
-    if (isset($_POST['submit'])) {
-        try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
-            // Insert data
-            $sql_insert = "INSERT INTO Pengguna (Nama, Email, Pekerjaan, Tanggal) 
-                        VALUES ($name,$email,$job,$date)";
-         if(mysqli_query($conn, $sql_insert)){
-            echo "<h3>Your're registered!</h3>";
-        }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-        
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Pengguna";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['Nama']."</td>";
-                    echo "<td>".$registrant['Email']."</td>";
-                    echo "<td>".$registrant['Pekerjaan']."</td>";
-                    echo "<td>".$registrant['Tanggal']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
+    <head>
+        <Title>Registration Form</Title>
+        <style type="text/css">
+            body { background-color: #fff; border-top: solid 10px #000;
+                color: #333; font-size: .85em; margin: 20; padding: 20;
+                font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
             }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-    }
- ?>
- </body>
- </html>
+            h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+            h1 { font-size: 2em; }
+            h2 { font-size: 1.75em; }
+            h3 { font-size: 1.2em; }
+            table { margin-top: 0.75em; }
+            th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+            td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+        </style>
+    </head>
+    <body>
+        <h1>Register here!</h1>
+        <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+        <form method="post" action="index.php" enctype="multipart/form-data" >
+            Name  <input type="text" name="nama" id="nama"/></br></br>
+            Email <input type="text" name="email" id="email"/></br></br>
+            Job <input type="text" name="pekerjaan" id="pekerjaan"/></br></br>
+            <input type="submit" name="submit" value="Submit" />
+            <input type="submit" name="load_data" value="Load Data" />
+        </form>
+        <?php
+            $servername = "rifazures.database.windows.net";
+            $username = "waynerog";
+            $password = "Alkmenes1197";
+            $dbname = "Pendaftaran";
+
+            if (isset($_POST['submit'])){
+
+                $nama = $_POST['nama'];
+                $email = $_POST['email'];
+                $pekerjaan = $_POST['pekerjaan'];
+                $tanggal = date("Y-m-d");
+
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                // Check connection
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "INSERT INTO Pengguna (Nama, Email, Pekerjaan, Tanggal)
+                VALUES ($nama, $email, $pekerjaan, $tanggal)";
+
+                if (mysqli_query($conn, $sql)) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            } else if (isset($_POST['load_data'])) {
+                try {
+                    $sql_select = "SELECT * FROM Pengguna";
+                    $stmt = $conn->query($sql_select);
+                    $registrants = $stmt->fetchAll(); 
+                    if(count($registrants) > 0) {
+                        echo "<h2>People who are registered:</h2>";
+                        echo "<table>";
+                        echo "<tr><th>Name</th>";
+                        echo "<th>Email</th>";
+                        echo "<th>Job</th>";
+                        echo "<th>Date</th></tr>";
+                        foreach($registrants as $registrant) {
+                            echo "<tr><td>".$registrant['Nama']."</td>";
+                            echo "<td>".$registrant['Email']."</td>";
+                            echo "<td>".$registrant['Pekerjaan']."</td>";
+                            echo "<td>".$registrant['Tanggal']."</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "<h3>No one is currently registered.</h3>";
+                    }
+                } catch(Exception $e) {
+                    echo "Failed: " . $e;
+                }
+            }
+
+
+            mysqli_close($conn);
+            ?> 
+    </body>
+</html>
