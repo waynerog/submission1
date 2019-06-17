0f4615768475f16e6a3db6358e530959e303254d
Rifqi Afif <rifqiafif007@gmail.com>
2019-06-17 14:14:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
             $date = date("Y-m-d");
             // Insert data
             $sql_insert = "INSERT INTO Pengguna (ID, Nama, Email, Pekerjaan, Tanggal) 
-                        VALUES (1,?,?,?,?)";
+                        VALUES (2,?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
             $stmt->bindValue(2, $email);
