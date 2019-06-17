383d08ce46aa96ef78646b0ad5e546a4495412d1
Rifqi Afif <rifqiafif007@gmail.com>
2019-06-17 14:14:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,8 +43,8 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Pengguna (Nama, Email, Pekerjaan, Tanggal) 
-                        VALUES (?,?,?,?)";
+            $sql_insert = "INSERT INTO Pengguna (ID, Nama, Email, Pekerjaan, Tanggal) 
+                        VALUES (1,?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
             $stmt->bindValue(2, $email);
